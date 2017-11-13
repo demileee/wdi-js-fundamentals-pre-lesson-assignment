42546d5a210cc9ff9ea0e40bec435af55d0dce7b
Demi Lee <demilee.to@gmail.com>
2017-11-12 23:06:00 -0500

    if else

---
+++ pacman-if-else.js
@@ -0,0 +1,40 @@
+// pacman-if-else.js
+var powerPelletEaten = false;
+var ghosts = 4;
+
+while (true) {
+  console.log('Ghosts remaining: ' + ghosts);
+
+  if (powerPelletEaten == false) {
+    console.log('Pac-Man eats the power pellet.');
+    powerPelletEaten = true;
+  } else if (ghosts > 0) {
+    console.log('Pac-Man eats a ghost.');
+    ghosts--;
+  } else {
+    break;
+  }
+}
+
+console.log('Pac-Man returns home after a long day in the maze.');
+
+// pacman-switch.js
+var ghost = 'Blinky';
+var colour;
+
+switch (ghost) {
+  case 'Inky':
+    colour = 'Cyan';
+    break;
+  case 'Blinky':
+    colour = 'Red';
+    break;
+  case 'Pinky':
+    colour = 'Pink';
+    break;
+  case 'Clyde':
+    colour = 'Orange';
+    break;
+}
+
+console.log(ghost + ' is the colour ' + colour);
